11842498d002427f7a47eb2067c0abe4eb50884b
BhusanDotel <110612485+BhusanDotel@users.noreply.github.com>
2023-09-22 09:42:44 +0545

    initial commit

--- todolist.js
+++ todolist.js
@@ -1,64 +1,76 @@
-        // let todolist=[
+        // const todolist = [
         //     {
-        //         task:'make dinner',
-        //         duedate:'2022-12-22'
-        //     }    //input data is saved as object inside array.
+        //         task:"Dancing",
+        //         date:"sunday",
+        //         time:"12/23"
+        //     }
         // ];
 
-        let todolist = JSON.parse(localStorage.getItem('todolist')) || [];
-        
-         function renderTask(){
-            let todolistHTML = "";
+        let todolist_jason = localStorage.getItem('todolist');
+        let todolist = (JSON.parse(todolist_jason) || []);
+        rednderTask();
 
-            todolist.forEach(function(todoObject, index){
-                const task = todoObject.task;
-                const duedate = todoObject.duedate;
-                const html = `
-                <div class="task_row">
-                    <div class="date_task_div">${task}</div>
-                    <div class="date_task_div">${duedate}</div>
-                    <button class="delete_btn" >Delete</button>
-                </div>
-                `;
-                todolistHTML = todolistHTML+html; 
-            });
-            document.querySelector(".js-todo-list").innerHTML=todolistHTML;
+        const addBtn = document.querySelector(".add-btn");
+        addBtn.addEventListener('click',()=>{
 
-            const del_button = document.querySelectorAll(".delete_btn");
-            del_button.forEach(function(deletebuttons,index){
-                deletebuttons.addEventListener('click',()=>{
-                todolist.splice(index,1);
-                renderTask();
-                });
-            });
-            
-            localStorage.setItem('todolist', JSON.stringify(todolist)); // Save the updated todolist in localStorage
+            let activityField = document.querySelector(".activity");
+            let duedateField = document.querySelector(".date");
+            let duetimeField = document.querySelector(".time");
 
-        }
+            let activity_in = activityField.value;
+            let duedate_in = duedateField.value;
+            let duetime_in = duetimeField.value;
 
-        const add_button = document.querySelector(".add_btn");
-        add_button.addEventListener('click',()=>{
-            Add();
-        })
-        
-        function Add(){
-            const activity_input = document.querySelector(".input_box").value;
-            const date_input = document.querySelector(".date_input_box").value;
+            if(activity_in!="" && duedate_in!="" && duetime_in!=""){
+                activityField.value="";
+                duedateField.value="";
+                duetimeField.value="";
 
-            if(activity_input==="" || date_input===""){
-                alert('Do not leave any field!')
-            }else{
                 todolist.push({
-                    task:activity_input,
-                    duedate:date_input
+                    task:activity_in,
+                    date:duedate_in,
+                    time:duetime_in
                 });
 
-                document.querySelector(".input_box").value="";
-                document.querySelector(".date_input_box").value="";
-
-                renderTask();
+                rednderTask();
+            }else{
+                alert("Do not leave fields Empty");
             }
 
+        });
+
+        function rednderTask(){
+
+            let JSON_todolist = JSON.stringify(todolist);
+            localStorage.setItem('todolist',JSON_todolist);
+
+            let HTML="";
+            todolist.forEach((ArrayItem,ArrayIndex)=>{
+                const activity = ArrayItem.task;
+                const date = ArrayItem.date;
+                const time = ArrayItem.time;
+
+                const html=`
+                    <div class="task-row">
+                    <p class="show_activity">${activity}</p>
+                    <p class="show-date">${date}</p>
+                    <p class="show-time">${time}</p>
+                    <button data-activity-id="${ArrayIndex}" class="Delete">Delete</button>
+                    </div>
+                `;
+                HTML= HTML+html;
+            });
+
+            const renderdiv = document.querySelector(".show-task");
+            renderdiv.innerHTML=HTML;
+
+            let delBtn = document.querySelectorAll(".Delete");
+            delBtn.forEach((button) =>{
+                button.addEventListener('click',() =>{
+                 const activityId=button.dataset.activityId;
+                 const index = parseInt(activityId);
+                todolist.splice(index,1);
+                rednderTask();
+                });
+            });
         }
-        //initial rendering of task, to show saved undeleted tasks.
-    renderTask();
